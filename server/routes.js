a942d44b0e8847c240f79560ed94fbefc5be771e
John Wesley Salvador <John Wesley Salvador>
2017-03-07 15:53:07 +1100

    access control allow origin

--- server/routes.js
+++ server/routes.js
@@ -5,19 +5,19 @@ const Guests = require('./controller/Guests');
 const RoutesConfig = (app) => {
 
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // restrict it to the required domain
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     // Set custom headers for CORS
     res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
     next();
   });
-  
+
   app.get('/api/guests', Guests.Get);
   app.post('/api/guest', Guests.Find);
   app.post('/api/guest/rsvp', Guests.Rsvp);
   app.post('/api/guests', Guests.Save);
   app.post('/api/guests/link', Guests.Link);
-  
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
   });
