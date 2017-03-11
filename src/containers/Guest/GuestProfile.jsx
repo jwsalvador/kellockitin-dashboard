39da275853b2a2c059bfb05ed4e54b3f8f455064
John Wesley Salvador <John Wesley Salvador>
2017-03-11 20:55:06 +1100

    fixed rsvp values

--- src/containers/Guest/GuestProfile.jsx
+++ src/containers/Guest/GuestProfile.jsx
@@ -12,7 +12,7 @@ const Profile = ({selected}) => {
     <div className="col-7" style={{textAlign: 'center', padding: '20px'}}>
       <div><SocialPerson style={{width: 150, height: 150}}/></div>
       <h2 className="name">{`${firstName} ${lastName}`}</h2>
-      <h3>{`RSVP: ${rsvp === 'yes' ? 'yes': rsvp === 'no' ? 'no' : 'No status yet'}`}</h3>
+      <h3>{`RSVP: ${rsvp === 'y' ? 'yes': rsvp === 'n' ? 'no' : 'No status yet'}`}</h3>
       <h3>{`Dietary Requirements: ${diet ? diet : 'No requirements provided'}`}</h3>
       <h3>{`Message: ${message ? message : 'No message provided'}`}</h3>
     </div>
